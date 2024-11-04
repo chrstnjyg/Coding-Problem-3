d48793b3ad595890fdd1dc8ea6c310ebf320e19c
Christine <guerrerochristinejoy15@gmail.com>
2024-11-04 22:20:10 +0800

    Initial commit

---
+++ ageCategory.js
@@ -0,0 +1,15 @@
+let age = "24";
+
+if (age < 5) {
+    console.log("Toddler: You are under 5 years old.");
+} else if (age <= 12) {
+    console.log("Child: You are between 5 and 12 years old.");
+} else if (age <= 19) {
+    console.log("Teenager: You are between 13 and 19 years old.");
+} else if (age <= 35) {
+    console.log("Young Adult: You are between 20 and 35 years old.");
+} else if (age <= 60) {
+    console.log("Middle-Aged: You are between 36 and 60 years old.");
+} else {
+    console.log("Senior: You are over 60 years old.");
+}
